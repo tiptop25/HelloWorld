6dc1c5e1f88e9603eafd7ee928f0eb2dd87cfa32
tiptop25 <melnik_af@mail.ru>
2015-10-30 23:13:44 +0200

    Домашнее задание

---
+++ src/main/java/Task5.java
@@ -0,0 +1,12 @@
+/**
+ * Created by superova on 30.10.2015.
+ */
+public class Task5 {
+    public static void main(String[] args) {
+
+        for (int i = 1, b = 1; i <= 55; i = i + 2,b++){
+
+            System.out.println(i);
+        }
+    }
+}
