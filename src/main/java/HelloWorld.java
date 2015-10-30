d616cdf300d298bac0d9d641ff3e0460f93ebdf3
tiptop25 <melnik_af@mail.ru>
2015-10-30 14:48:52 +0200

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Created by superova on 04.10.2015.
+ */
+public class HelloWorld {
+    public static void main(String... args) {
+       double b1,b2 =4.12;
+
+
+      double d =3.14;
+        int a =(int)d;
+        System.out.println(a);
+        int i  = 2015;
+        float f =(float)i;
+        System.out.println(i);
+        int k;
+        int c = k = -3;
+        k =5;
+        System.out.println(c);
+
+
+    }
+}
---
+++ src/main/java/Task1.java
@@ -0,0 +1,6 @@
+/**
+ * Created by superova on 30.10.2015.
+ */
+public class Task1 {
+    
+}
